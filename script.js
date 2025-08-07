200297dba2abea2bc24de1715dfdfacd4d15ab69
sachinzx12 <sachinzx18@gmail.com>
2025-08-07 14:18:28 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-var display = document.getElementById("display");
-function appendToDisplay(value) {
-    display.value += value;
-}
-function clearDisplay() {
-    display.value = "";
-}
-function calculate() {
-    try {
-        display.value = eval(display.value).toString();
-    }
-    catch (e) {
-        display.value = "Error";
-    }
+function submit(){
+    var Fullname = document.getElementById("Fullname");
+    console.log(Fullname .value);
+    var Fathername = document.getElementById("Fathername");
+    console.log(Fathername .value);
+    var Mothername = document.getElementById("Mothername");
+    console.log(Mothername .value);
+    var Age = document.getElementById("Age");
+    console.log(Age .value);
+    var Dob = document.getElementById("Dob");
+    console.log(Dob .value);
+    var Address = document.getElementById("Address");
+    console.log(Address .value);
+    var Email = document.getElementById("Email");
+    console.log(Email .value);
+    var phno = document.getElementById("phno");
+    console.log(phno .value);
+    var Course = document.getElementById("Course");
+    console.log(Course .value);
+    var bloodgroup = document.getElementById("bloodgroup");
+    console.log(bloodgroup .value);
+    var x = document.getElementById("male");
+    var y = document.getElementById("female");
+    var z = document.getElementById("others");
+
 }
